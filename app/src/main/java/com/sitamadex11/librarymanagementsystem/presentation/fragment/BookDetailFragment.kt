dba287039f4839e90d73a978a16b651ff278bea0
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2022-04-21 08:07:22 +0530

    Book Detail fragment added

---
+++ app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/fragment/BookDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.sitamadex11.librarymanagementsystem.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sitamadex11.librarymanagementsystem.R
+
+class BookDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
