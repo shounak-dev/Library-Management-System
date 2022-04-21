626dbbd44aa9e0465ef8a83d449cdb148e569db0
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2022-04-22 01:32:49 +0530

    updated

--- app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/adapter/recyclerview/BookDetailRecyclerAdapter.kt
+++ app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/adapter/recyclerview/BookDetailRecyclerAdapter.kt
@@ -1,30 +1,46 @@
 package com.sitamadex11.librarymanagementsystem.presentation.adapter.recyclerview
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sitamadex11.librarymanagementsystem.R
+import com.sitamadex11.librarymanagementsystem.data.model.BookDetail
 
-class BookDetailRecyclerAdapter:
+class BookDetailRecyclerAdapter():
     RecyclerView.Adapter<BookDetailRecyclerAdapter.BookDetailRecyclerViewHolder>() {
-
+    private val list = ArrayList<BookDetail>()
+    lateinit var context:Context
     inner class BookDetailRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+           val imgBook: ImageView = itemView.findViewById(R.id.imgBook)
+           val txtBookName: TextView = itemView.findViewById(R.id.txtBookName)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): BookDetailRecyclerViewHolder {
+        context = parent.context
         val view = LayoutInflater.from(parent.context).inflate(R.layout.book_list_item,parent,false)
         return BookDetailRecyclerViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: BookDetailRecyclerViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BookDetailRecyclerViewHolder, pos: Int) {
+        Glide.with(context).load(list[pos].imgUrl).into(holder.imgBook)
+        holder.txtBookName.text = list[pos].title
     }
 
     override fun getItemCount(): Int {
-        return 10
+        return list.size
+    }
+
+    fun updateList(newList: List<BookDetail>){
+        list.clear()
+        list.addAll(newList)
+        notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/fragment/HomeFragment.kt
+++ app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/fragment/HomeFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.sitamadex11.librarymanagementsystem.R
+import com.sitamadex11.librarymanagementsystem.data.model.BookDetail
 import com.sitamadex11.librarymanagementsystem.databinding.FragmentHomeBinding
 import com.sitamadex11.librarymanagementsystem.presentation.adapter.recyclerview.BookCategoryRecyclerAdapter
 import com.sitamadex11.librarymanagementsystem.presentation.adapter.recyclerview.BookDetailRecyclerAdapter
@@ -19,6 +20,7 @@ import com.sitamadex11.librarymanagementsystem.presentation.adapter.recyclerview
 class HomeFragment : Fragment() {
     val TAG = "chkFirestore"
     lateinit var binding: FragmentHomeBinding
+    val bookList = ArrayList<BookDetail>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,25 +42,68 @@ class HomeFragment : Fragment() {
         binding.rvTopics.layoutManager = LinearLayoutManager(requireContext(),RecyclerView.HORIZONTAL,false)
         binding.rvTopics.adapter = bookCategoryAdapter
 
-        val DSA = hashMapOf(
-            "201" to hashMapOf(
-                "author" to arrayOf("Armstrong Subero"),
-                "avlQty" to 10,
-                "date" to "2020",
-                "desc" to "In the era of self-taught developers and programmers, essential topics in the industry are frequently learned without a formal academic foundation. A solid grasp of data structures and algorithms (DSA) is imperative for anyone looking to do professional software development and engineering, but classes in the subject can be dry or spend too much time on theory and unnecessary readings. Regardless of your programming language background, Codeless Data Structures and Algorithms has you covered. In this book, author Armstrong Subero will help you learn DSAs without writing a single line of code. Straightforward explanations and diagrams give you a confident handle on the topic while ensuring you never have to open your code editor, use a compiler, or look at an integrated development environment. Subero introduces you to linear, tree, and hash data structures and gives you important insights behind the most common algorithms that you can directly apply to your own programs. Codeless Data Structures and Algorithms provides you with the knowledge about DSAs that you will need in the professional programming world, without using any complex mathematics or irrelevant information. Whether you are a new developer seeking a basic understanding of the subject or a decision-maker wanting a grasp of algorithms to apply to your projects, this book belongs on your shelf. Quite often, a new, refreshing, and unpretentious approach to a topic is all you need to get inspired. What You'll Learn Understand tree data structures without delving into unnecessary details or going into too much theory Get started learning linear data structures with a basic discussion on computer memory Study an overview of arrays, linked lists, stacks and queues Who This Book Is ForThis book is for beginners, self-taught developers and programmers, and anyone who wants to understand data structures and algorithms but don’t want to wade through unnecessary details about quirks of a programming language or don’t have time to sit and read a massive book on the subject. This book is also useful for non-technical decision-makers who are curious about how algorithms work.",
-                "imgUrl" to "http://books.google.com/books/content?id=GXvQDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
-                "page" to "146",
-                "title" to "Codeless Data Structures and Algorithms",
-                "totalQty" to 12
-            )
-        )
+//        val dsa = hashMapOf(
+//                "id" to 201,
+//                "author" to listOf("Armstrong Subero"),
+//                "avlQty" to 10,
+//                "date" to "2020",
+//                "desc" to "",
+//                "imgUrl" to "",
+//                "page" to "146",
+//                "title" to "Codeless Data Structures and Algorithms",
+//                "totalQty" to 12,
+//                "category" to "dsa"
+//        )
+//        val c = hashMapOf(
+//            "id" to 201,
+//            "author" to listOf("Brian W. Kernighan","Dennis M. Ritchie"),
+//            "avlQty" to 9,
+//            "date" to "2020",
+//            "desc" to "Introduces the features of the C programming language, discusses data types, variables, operators, control flow, functions, pointers, arrays, and structures, and looks at the UNIX system interface",
+//            "imgUrl" to "http://books.google.com/books/content?id=OpJ_0zpF7jIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+//            "page" to "146",
+//            "title" to "The C Programming Language",
+//            "totalQty" to 12,
+//            "category" to "dsa"
+//        )
+
+//        val arr = arrayOf(dsa,c)
+//
+//        repeat(2){
+//            db.collection("books")
+//                .add(arr[it])
+//                .addOnSuccessListener { documentReference ->
+//                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//                }
+//                .addOnFailureListener { e ->
+//                    Log.w(TAG, "Error adding document", e)
+//                }
+//        }
+
+
         db.collection("books")
-            .add(DSA)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            .get()
+            .addOnSuccessListener { result ->
+                bookList.clear()
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    bookList.add(BookDetail(
+                        document.get("id") as Long,
+                        document.get("authors") as List<String>,
+                        document.get("avlQty") as Int,
+                        document.get("date") as String,
+                        document.get("desc") as String,
+                        document.get("imgUrl") as String,
+                        document.get("page") as String,
+                        document.get("title") as String,
+                        document.get("totalQty") as Int,
+                        document.get("category") as String
+                    ))
+                }
+                bookDetailAdapter.updateList(bookList)
             }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
             }
     }
 }
