b1e0a17362a5ba5b6fa24b637ad990aa79582bd7
Shounak Das <shounakdas30@gmail.com>
2022-04-22 06:06:34 +0530

    Add Re-Issue & Return Book Fragment (#2)
    * Add Issue Book Fragment
    
    * Add Re-Issue & Return Book Fragment
    
    * Add Re-Issue & Return Book Fragment
    
    Co-authored-by: Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>

--- app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/fragment/ReIssueBookFragment.kt
+++ app/src/main/java/com/sitamadex11/librarymanagementsystem/presentation/fragment/ReIssueBookFragment.kt
@@ -1,15 +1,15 @@
 package com.sitamadex11.librarymanagementsystem.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.sitamadex11.librarymanagementsystem.R
 import com.sitamadex11.librarymanagementsystem.databinding.FragmentIssueBookBinding
 
-class IssueBookFragment : Fragment() {
+class ReIssueBookFragment : Fragment() {
     private lateinit var binding: FragmentIssueBookBinding
 
     override fun onCreateView(
@@ -17,14 +17,13 @@ class IssueBookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_issue_book, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_issue_book, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.pageTitle = "ISSUE BOOK"
-        binding.buttonTitle = "ISSUE"
+        binding.pageTitle = "Re-ISSUE BOOK"
+        binding.buttonTitle = "Re-ISSUE"
     }
 }
